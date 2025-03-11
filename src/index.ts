a31ae7b940af982cacad8ff75d95dbd0c8a6940f
Francis Villarba <86995338+FrancisVillarba@users.noreply.github.com>
2025-03-11 01:41:58 +0000

    feat: Base Template

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * The main entrypoint for linear-gha module
+ */
+
+const main = async () => {
+    // Fancy Header
+    console.log(" ");
+    console.log("===== ADD A TITLE HERE ts-project-base =====");
+    console.log("Written by Francis Villarba <hello@francisvillarba.com>");
+    console.log(" ");
+
+};
+
+main();
